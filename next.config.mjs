1e9c17ddccd2b11e58d38108cca2e944b92fcb45
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2024-02-06 14:56:41 +0000

    Starter Template

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
-/**
- * @type {import('next').NextConfig}
- */
+// next.config.mjs
+
 const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  // your Next.js configuration options here
 }
- 
-module.exports = nextConfig
+
+export default nextConfig;
