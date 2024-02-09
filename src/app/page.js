d79b7260991f63cfd8ab21e4dcdb101e12d0d5a2
SH20RAJ <shaswatraj3@gmail.com>
2024-02-09 11:56:37 +0530

    rankings

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// pages/edit.js
 
-export default function Home() {
-  return (
-    <></>
-  );
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/index.html');
+  }, []);
+
+  return null; // This component doesn't render anything, it only performs the redirection
 }
