0f47b41a6c9c942c31e7f672b164ba8637dbdd9f
SH20RAJ <shaswatraj3@gmail.com>
2024-02-09 11:04:11 +0530

    with errs

--- src/app/post/[id]/footer.js
+++ src/app/post/[id]/footer.js
@@ -1,3 +1,4 @@
+import Script from "next/script"
 export default function Footer(){
     return (
         <>
@@ -18,6 +19,17 @@ export default function Footer(){
       <div className="clearfix"></div>
     </div>
   </div>
+
+
+        <Script src="../site.js"></Script>
+        <Script src="../assets/js/jquery.min.js"></Script>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></Script>
+        <Script src="../assets/js/bootstrap.min.js"></Script>
+        <Script src="../assets/js/ie10-viewport-bug-workaround.js"></Script>
+        <Script src="../assets/js/mediumish.js"></Script>
+        <Script src="https://cdn.jsdelivr.net/npm/prismjs@1.25.0/prism.min.js"></Script>
+        <Script src="../post.js"></Script>
+
 </>
 
     )
